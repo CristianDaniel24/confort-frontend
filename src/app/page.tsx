197bbab0bcf90d04211b62e7789d24fafc8cd1cf
CristianDaniel24 <cristianponguta789@gmail.com>
2025-03-26 07:06:04 -0500

    feat: Creacion del proyecto con React

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col min-h-screen items-center justify-center">
+      <h1>Tapiceria Confort</h1>
+    </div>
+  );
+}
