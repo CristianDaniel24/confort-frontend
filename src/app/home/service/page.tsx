6794f8fda819b64b8eef27dd4006eb14320a264b
CristianDaniel24 <cristianponguta789@gmail.com>
2025-06-19 16:21:09 -0500

    refactor: configuraciones finales

--- src/app/home/service/page.tsx
+++ src/app/home/service/page.tsx
@@ -1,16 +1,38 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { DataTable } from "./data-table";
 import { columns } from "./columns";
 import { serviceService } from "@/services/service.service";
+import { IService } from "@/types/service-interface";
 
-export default async function Services() {
-  const service = await serviceService.getAll();
+export default function Services() {
+  const [services, setServices] = useState<IService[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchServices = async () => {
+      try {
+        const data = await serviceService.getAll();
+        setServices(data);
+      } catch (error) {
+        console.error("Error al obtener los servicios:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchServices();
+  }, []);
+
+  if (loading) {
+    return <div className="p-4">Cargando servicios...</div>;
+  }
 
   return (
     <div className="container max-w-5xl mx-auto py-10">
       <h1 className="text-4xl leading-none font-medium">Lista de servicios:</h1>
-      <DataTable columns={columns} data={service} />
+      <DataTable columns={columns} data={services} />
     </div>
   );
 }
