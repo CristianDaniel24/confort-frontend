62e7a4cb1c7bbe081c5c34f7fa0f215022a4c263
CristianDaniel24 <cristianponguta789@gmail.com>
2025-06-18 19:02:17 -0500

    refactor: configuraciones finales

--- src/app/home/client/page.tsx
+++ src/app/home/client/page.tsx
@@ -1,11 +1,23 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { DataTable } from "./data-table";
 import { columns } from "./columns";
 import { clientService } from "@/services/client.service";
+import { IClient } from "@/types/client-interface";
 
-export default async function Client() {
-  const client = await clientService.getAll();
+export default function Client() {
+  const [client, setClient] = useState<IClient[]>([]);
+
+  useEffect(() => {
+    // Llama a la API cuando el componente se monta
+    clientService
+      .getAll()
+      .then((data) => setClient(data))
+      .catch((error) => {
+        console.error("Error al cargar los clientes:", error);
+      });
+  }, []);
 
   return (
     <div className="container max-w-5xl mx-auto py-10">
